c4f662f53b6401c07c267397663a26a8d16d721c
Lubi2003 <lucaspub2003@gmail.com>
2024-04-12 11:53:08 +0100

    Mission 5 : assurer la sécurité, la qualité et intégrer de logs

--- MediaTekDocuments/dal/Access.cs
+++ MediaTekDocuments/dal/Access.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using System.Configuration;
+using Serilog;
 
 namespace MediaTekDocuments.dal
 {
@@ -17,7 +18,11 @@ namespace MediaTekDocuments.dal
         /// <summary>
         /// adresse de l'API
         /// </summary>
-        private static readonly string uriApi = "http://localhost/rest_mediatekdocuments/";
+        private static readonly string uriApiName = "MediaTekDocuments.Properties.Settings.mediatekConnectionString";
+        /// <summary>
+        /// pwd et login de l'API
+        /// </summary>
+        private static readonly string authenticationName = "MediaTekDocuments.Properties.Settings.mediatekAuthenticationString";
         /// <summary>
         /// instance unique de la classe
         /// </summary>
@@ -49,14 +54,20 @@ namespace MediaTekDocuments.dal
         /// </summary>
         private Access()
         {
-            String authenticationString;
             try
             {
-                authenticationString = "admin:adminpwd";
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Verbose()
+                    .WriteTo.Console()
+                    .WriteTo.File("logs/log.txt")
+                    .CreateLogger();
+                String authenticationString = GetAuthentificationString(authenticationName);
+                String uriApi = GetAuthentificationString(uriApiName);
                 api = ApiRest.GetInstance(uriApi, authenticationString);
             }
             catch (Exception e)
             {
+                Log.Fatal("Access catch erreur={0}", e.Message);
                 Console.WriteLine(e.Message);
                 Environment.Exit(0);
             }
@@ -76,20 +87,38 @@ namespace MediaTekDocuments.dal
         }
 
         /// <summary>
-        /// 
+        /// Récupération de la chaîne de connexion
         /// </summary>
-        /// <param name="mail"></param>
-        /// <param name="hash"></param>
+        /// <param name="name"></param>
         /// <returns></returns>
-        public Utilisateur GetLogin(string mail, string hash)
+        static string GetAuthentificationString(string name)
         {
-            Dictionary<string, string> login = new Dictionary<string, string>();
-            login.Add("mail", mail);
-            login.Add("password", hash);
+            string returnValue = null;
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+            return returnValue;
+        }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="mail"></param>
+    /// <param name="hash"></param>
+    /// <returns></returns>
+    public Utilisateur GetLogin(string mail, string hash)
+        {
+            Dictionary<string, string> login = new Dictionary<string, string>
+            {
+                { "mail", mail },
+                { "password", hash }
+            };
             String mailHash = JsonConvert.SerializeObject(login);
             List<Utilisateur> utilisateurs = TraitementRecup<Utilisateur>(GET, "utilisateur/" + mailHash);
             if (utilisateurs.Count > 0)
                 return utilisateurs[0];
+            Log.Error("Access.GetLogin catch user fail connection :" + mail);
             return null;
         }
 
@@ -168,6 +197,7 @@ namespace MediaTekDocuments.dal
             }
             catch (Exception ex)
             {
+                Log.Error("Access.CreerEntite catch type erreur={0}, table={1}, champs={2}", ex, type, jsonEntite);
                 Console.WriteLine(ex.Message);
             }
             return false;
@@ -189,6 +219,7 @@ namespace MediaTekDocuments.dal
             }
             catch (Exception ex)
             {
+                Log.Error("Access.UpdateEntite catch type erreur={0}, table={1}, champs={2}", ex, type, jsonEntite);
                 Console.WriteLine(ex.Message);
             }
             return false;
@@ -209,6 +240,7 @@ namespace MediaTekDocuments.dal
             }
             catch (Exception ex)
             {
+                Log.Error("Access.SupprimerEntite catch type erreur={0}, table={1}, champs={2}", ex, type, jsonEntite);
                 Console.WriteLine(ex.Message);
             }
             return false;
@@ -243,7 +275,7 @@ namespace MediaTekDocuments.dal
         /// <returns>Liste d'objets Exemplaire</returns>
         public List<Exemplaire> GetExemplairesRevue(string idDocument)
         {
-            String jsonIdDocument = convertToJson("id", idDocument);
+            String jsonIdDocument = ConvertToJson("id", idDocument);
             List<Exemplaire> lesExemplaires = TraitementRecup<Exemplaire>(GET, "exemplaire/" + jsonIdDocument);
             return lesExemplaires;
         }
@@ -255,7 +287,7 @@ namespace MediaTekDocuments.dal
         /// <returns></returns>
         public List<CommandeDocument> GetCommandesLivres(string idLivre)
         {
-            String jsonIdDocument = convertToJson("idLivreDvd", idLivre);
+            String jsonIdDocument = ConvertToJson("idLivreDvd", idLivre);
             List<CommandeDocument> lesCommandesLivres = TraitementRecup<CommandeDocument>(GET, "commandedocument/" + jsonIdDocument);
             return lesCommandesLivres;
         }
@@ -267,7 +299,7 @@ namespace MediaTekDocuments.dal
         /// <returns></returns>
         public List<Abonnement> GetAbonnements(string idRevue)
         {
-            String jsonAbonnementIdRevue = convertToJson("idRevue", idRevue);
+            String jsonAbonnementIdRevue = ConvertToJson("idRevue", idRevue);
             List<Abonnement> abonnements =  TraitementRecup<Abonnement>(GET, "abonnements/" + jsonAbonnementIdRevue);
             return abonnements;
         }
@@ -278,7 +310,7 @@ namespace MediaTekDocuments.dal
         /// </summary>
         /// <param name="maxIndex"></param>
         /// <returns></returns>
-        public string getMaxIndex(string maxIndex)
+        public string GetMaxIndex(string maxIndex)
         {
             List<Categorie> maxindex = TraitementRecup<Categorie>(GET, maxIndex);
             return maxindex[0].Id;
@@ -299,6 +331,7 @@ namespace MediaTekDocuments.dal
             }
             catch (Exception ex)
             {
+                Log.Error("Access.CreerExemplaire catch type erreur={0} champs={1}", ex, jsonExemplaire);
                 Console.WriteLine(ex.Message);
             }
             return false; 
@@ -333,10 +366,12 @@ namespace MediaTekDocuments.dal
                 else
                 {
                     Console.WriteLine("code erreur = " + code + " message = " + (String)retour["message"]);
+                    Log.Error("Access.TraitementRecup code erreur = " + code + " message = " + (String)retour["message"]);
                 }
             }catch(Exception e)
             {
                 Console.WriteLine("Erreur lors de l'accès à l'API : "+e.Message);
+                Log.Fatal("Erreur lors de l'accès à l'API : " + e.Message);
                 Environment.Exit(0);
             }
             return liste;
@@ -348,10 +383,12 @@ namespace MediaTekDocuments.dal
         /// <param name="nom"></param>
         /// <param name="valeur"></param>
         /// <returns>couple au format json</returns>
-        private String convertToJson(Object nom, Object valeur)
+        private String ConvertToJson(Object nom, Object valeur)
         {
-            Dictionary<Object, Object> dictionary = new Dictionary<Object, Object>();
-            dictionary.Add(nom, valeur);
+            Dictionary<Object, Object> dictionary = new Dictionary<Object, Object>
+            {
+                { nom, valeur }
+            };
             return JsonConvert.SerializeObject(dictionary);
         }
 
